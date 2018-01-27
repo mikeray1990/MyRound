6ce8383df844321bf64e868e382ce237e4064b93
mike.ray <mikeray1990@gmail.com>
2018-01-27 18:35:19 +0000

    Add basic add/remove count

--- app/src/main/java/com/mikeray/myround/MainActivity.java
+++ app/src/main/java/com/mikeray/myround/MainActivity.java
@@ -1,13 +1,30 @@
 package com.mikeray.myround;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    public void addOrder(View view) {
+        TextView item1Count = findViewById(R.id.item1Count);
+        int count = Integer.parseInt(item1Count.getText().toString());
+        count++;
+        item1Count.setText(String.valueOf(count));
+    }
+
+    public void removeOrder(View view) {
+        TextView item1Count = findViewById(R.id.item1Count);
+        int count = Integer.parseInt(item1Count.getText().toString());
+        if (count > 0) {
+            count--;
+        }
+        item1Count.setText(String.valueOf(count));
     }
 }
